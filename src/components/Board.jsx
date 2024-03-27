7faaba2036ba1de5bd25231a41eeb0f7701be9f1
ssihvola <sihvola@protonmail.com>
2024-03-27 10:07:44 +0200

    Add comments to Board component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,10 +3,13 @@ import Block from "./Block";
 const Board = ({ gameGrid }) => {
   return (
 		<div className="gameGrid">
+			{/* Mapping over each row in the gameGrid array */}
 			{gameGrid.map((row, rowIndex) => {
 				return (
 					<div className="gameRow" key={rowIndex}>
+						{/* Mapping over each digit in the current row */}
 						{row.map((digit, columnIndex) => (
+							// Rendering a Block component for each digit in the row
 							<Block key={columnIndex} digit={digit === 0 ? null : digit} />
 						))}
 					</div>
