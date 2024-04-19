8cf750a39cf1fb33a7aa14ea32e04d31ee51d13e
ssihvola <sihvola@protonmail.com>
2024-04-19 11:47:41 +0300

    Refactor event handling into Game2048

--- src/swipes/swipeLeft.js
+++ src/swipes/swipeLeft.js
@@ -1,47 +1,36 @@
 import cloneDeep from 'lodash.clonedeep';
-import addNumber from '../utils/addNumber';
 import pointCalculation from '../utils/pointCalculation';
-import isGameOver from '../utils/isGameOver';
 
-const swipeLeft = ({ setGameGrid }) => {
-	setGameGrid((gameGrid) => {
-		let newGrid = cloneDeep(gameGrid);
+const swipeLeft = (gameGrid) => {
+	let newGrid = cloneDeep(gameGrid);
 
-		// Iterate through each row of the grid
-		for (let row = 0; row < newGrid.length; row++) {
-			// Iterate through each column from left to right in the current row
-			for (let col = 0; col < newGrid[row].length; col++) {
-				// If the current cell is not empty (contains a number)
-				if (newGrid[row][col] !== 0) {
-					let currentCol = col;
-					// Move the current number to the left as far as possible
-					while (currentCol - 1 >= 0 && newGrid[row][currentCol - 1] === 0) {
-						newGrid[row][currentCol - 1] = newGrid[row][currentCol];
-						newGrid[row][currentCol] = 0;
-						currentCol--;
-					}
-					// If the next cell to the left has the same number, combine them
-					if (
-						currentCol - 1 < newGrid[row].length &&
-						newGrid[row][currentCol - 1] === newGrid[row][currentCol]
-					) {
-						let number = (newGrid[row][currentCol - 1] *= 2);
-						pointCalculation(number);
-						newGrid[row][currentCol] = 0;
-					}
+	// Iterate through each row of the grid
+	for (let row = 0; row < newGrid.length; row++) {
+		// Iterate through each column from left to right in the current row
+		for (let col = 0; col < newGrid[row].length; col++) {
+			// If the current cell is not empty (contains a number)
+			if (newGrid[row][col] !== 0) {
+				let currentCol = col;
+				// Move the current number to the left as far as possible
+				while (currentCol - 1 >= 0 && newGrid[row][currentCol - 1] === 0) {
+					newGrid[row][currentCol - 1] = newGrid[row][currentCol];
+					newGrid[row][currentCol] = 0;
+					currentCol--;
+				}
+				// If the next cell to the left has the same number, combine them
+				if (
+					currentCol - 1 < newGrid[row].length &&
+					newGrid[row][currentCol - 1] === newGrid[row][currentCol]
+				) {
+					let number = (newGrid[row][currentCol - 1] *= 2);
+					pointCalculation(number);
+					newGrid[row][currentCol] = 0;
 				}
 			}
 		}
-		// Check if there was any movement in the grid
-		if (JSON.stringify(gameGrid) !== JSON.stringify(newGrid)) {
-			addNumber(newGrid);
-		}
-		if (isGameOver(newGrid)) {
-			alert('game over');
-		}
+	}
 
-		return newGrid;
-	});
+	return newGrid;
 };
 
 export default swipeLeft;
--- src/utils/useSwipe.js
+++ src/utils/useSwipe.js
@@ -3,23 +3,19 @@ import swipeLeft from '../swipes/swipeLeft';
 import swipeRight from '../swipes/swipeRight';
 import swipeUp from '../swipes/swipeUp';
 
-const useSwipe = ({ setGameGrid }) => {
-	const handleKeyDown = (event) => {
-		if (event.code === 'ArrowUp') {
-			swipeUp({ setGameGrid });
-		}
-		if (event.code === 'ArrowLeft') {
-			swipeLeft({ setGameGrid });
-		}
-		if (event.code === 'ArrowRight') {
-			swipeRight({ setGameGrid });
-		}
-		if (event.code === 'ArrowDown') {
-			swipeDown({ setGameGrid });
-		}
-	};
-
-	return { handleKeyDown };
+const useSwipe = (swipe, gameGrid) => {
+	if (swipe === 'ArrowUp') {
+		return swipeUp(gameGrid);
+	}
+	if (swipe === 'ArrowLeft') {
+		return swipeLeft(gameGrid);
+	}
+	if (swipe === 'ArrowRight') {
+		return swipeRight(gameGrid);
+	}
+	if (swipe === 'ArrowDown') {
+		return swipeDown(gameGrid);
+	}
 };
 
 export default useSwipe;
