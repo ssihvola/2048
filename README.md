0106750e1e95d13a6acba4b81fedb7ba4d708c35
ssihvola <sihvola@protonmail.com>
2024-04-25 11:03:37 +0300

    Config Cypress & add test cases into todo

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ This is my rendition of the game 2048, originally created by Gabrielle Cirulli i
 # To do
 
 - E2E tests
+	- How to test that two numbers get added together?
+	- Gameover situation
+	- Points: should not be zero after combining two numbers
 - Add usability test documentation into this doc
--- cypress/e2e/game2048.cy.js
+++ cypress/e2e/game2048.cy.js
@@ -1,18 +1,23 @@
 describe('Gameplay', () => {
-  it('opens up the site correctly', () => {
-    cy.visit('http://localhost:5173/')
-  })
+	beforeEach(() => {
+		cy.visit('');
+	});
 
-  it('can make moves using arrow keys', () => {
-    // Make sure the game is loaded
-    cy.visit('http://localhost:5173/');
+	it('opens up the site correctly', () => {
+		cy.contains('2048');
+    cy.get('.gameGrid').should('exist');
+	});
 
-    // Define arrow key commands
-    const arrowKeys = ['upArrow', 'downArrow', 'leftArrow', 'rightArrow'];
+	it('can make moves using arrow keys', () => {
+		// Make sure the game is loaded
+		cy.visit('http://localhost:5173/');
 
-    // Iterate over arrow keys and trigger keydown event
-    arrowKeys.forEach((key) => {
-      cy.get('body').type(`{${key}}`);
-    });
-  });
-})
+		// Define arrow key commands
+		const arrowKeys = ['upArrow', 'downArrow', 'leftArrow', 'rightArrow'];
+
+		// Iterate over arrow keys and trigger keydown event
+		arrowKeys.forEach((key) => {
+			cy.get('body').type(`{${key}}`);
+		});
+	});
+});
